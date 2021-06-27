1cd62ebf85d572622ba482db08602987b7ba1fd6
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-27 02:33:57 -0300

    Alterações no application.yml

--- src/main/java/com/dio/controle/acesso/AcessoApplication.java
+++ src/main/java/com/dio/controle/acesso/AcessoApplication.java
@@ -2,12 +2,28 @@ package com.dio.controle.acesso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class AcessoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcessoApplication.class, args);
+	}
+	
+	/* Metodo para Realizar a Documentação com OpenAPI e Swagger */
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+				.title("Ponto de Acesso API")
+				.version("1.0")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
